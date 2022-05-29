b76b3e95af92dbb8da41bff87c1e2df9d56a6a47
chandan kumar NA <33998599+chandankumar-na@users.noreply.github.com>
2022-05-29 13:17:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ basedir=os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_DATABASE_URI = 'sqlite:///'+os.path.join(basedir, 'db.sqlite3')
-# SQLALCHEMY_DATABASE_URI = "postgresql://postgres:password@localhost/cerberus"
+# SQLALCHEMY_DATABASE_URI = "postgresql://postgres:password@localhost/db_name"
